9b906c44c6a142875e14f90f5838a84378b243e4
Manas Mishra <92935580+ManasMishra09@users.noreply.github.com>
2023-10-30 14:51:19 +0530

    Add files via upload

---
+++ Book-Store-MERN-Stack/backend/config.js
@@ -0,0 +1,8 @@
+export const PORT = 5555;
+
+export const mongoDBURL =
+  'mongodb+srv://root:root@books-store-mern.c7ba8z1.mongodb.net/books-collection?retryWrites=true&w=majority';
+
+// export const mongoDBURL =
+//   'mongodb+srv://Manasmishra:qzqcHL@7!6J8xix@cluster0.nltsxku.mongodb.net/books-collection?retryWrites=true&w=majority';
+
